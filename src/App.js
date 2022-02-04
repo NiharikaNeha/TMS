0b863b859899ce642cb2364d7b8bbfd4776dc6c1
NiharikaNeha <45658446+NiharikaNeha@users.noreply.github.com>
2022-02-04 12:38:27 +0530

    added files and running basic react project

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Talent Management System</h1>
+      <h2>Bootcamp</h2>
+    </div>
+  );
+}
