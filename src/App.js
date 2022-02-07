e4351fae58eb808d3c067397623e47a3e40c3ad1
NiharikaNeha <45658446+NiharikaNeha@users.noreply.github.com>
2022-02-07 17:42:27 +0530

    added profile page ui

--- src/App.js
+++ src/App.js
@@ -1,10 +1,70 @@
 import "./styles.css";
+import { React} from "react";
+// import { tab } from "@testing-library/user-event/dist/tab";
 
 export default function App() {
+  // var a = 2;
+
   return (
     <div className="App">
-      <h1>Talent Management System</h1>
-      <h2>Bootcamp project for OYO</h2>
+      <table>
+        <tbody>
+          <th className="left-menu-bar">
+            <tr>
+              <p className="my-profile-text">My Profile</p>
+            </tr>
+            <tr>
+              <th>
+                <p className='review'>Review</p>
+              </th>
+            </tr>
+            <tr>
+              <p className='review-results'>Review Results</p>
+            </tr>
+          </th>
+          <th className="right-info-page">
+            <tr className="profile-bar">
+              <th className="name-mail-section">
+                <tr><p className="name">James Corden</p></tr>
+                <tr className="mail">oyo1@oyorooms.com</tr>
+              </th>
+              <th><img className="profile-photo" src="https://thumbs.dreamstime.com/b/profile-icon-vector-male-user-person-avatar-flat-color-glyph-pictogram-illustration-117610301.jpg"></img></th>
+            </tr>
+            <tr className="dept-desg-bar">
+              <th className="dept-bar">
+                <th><img className="photo" src="https://cdn1.vectorstock.com/i/1000x1000/18/85/department-structure-icon-in-trendy-flat-style-vector-30501885.jpg"></img></th>
+                <th className="title-description">
+                  <tr><p className="title">Department</p></tr>
+                  <tr><p className="description">SDE</p></tr>
+                </th>
+              </th>
+              <th className="desg-bar">
+                <th><img className="photo" src="https://as2.ftcdn.net/v2/jpg/03/05/75/77/1000_F_305757777_8zQDaRz3Y9tW8Jpd0jdC5NQoCQPgav5Z.jpg"></img></th>
+                <th className="title-description">
+                  <tr><p className="title">Designation</p></tr>
+                  <tr><p className="description">Employee</p></tr>
+                </th>
+              </th>
+            </tr>
+            <tr className="dept-desg-bar">
+              <th className="dept-bar">
+                <th><img className="photo" src="https://as1.ftcdn.net/v2/jpg/02/24/47/16/1000_F_224471646_jDVPhDImzvvQAejjW9bFfPOwcqnPr6sQ.jpg"></img></th>
+                <th className="title-description"> 
+                  <tr><p className="title">Date of Birth</p></tr>
+                  <tr><p className="description">07-10-1984</p></tr>
+                </th>
+              </th>
+              <th className="manager-bar">
+              <th><img className="photo" src="https://cdn3.vectorstock.com/i/1000x1000/70/52/executive-manager-icon-simple-style-vector-27507052.jpg"></img></th>
+              <th className="title-description">
+                  <tr><p className="title">Manager Name</p></tr>
+                  <tr><p className="description">Torben Devin</p></tr>
+                </th>
+              </th>
+            </tr>
+          </th>
+        </tbody>
+      </table>
     </div>
   );
 }
