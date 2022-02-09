ddedc04df8306236b8ced5cb74d299857494d879
NiharikaNeha <98871817+NiharikaKesarwani99@users.noreply.github.com>
2022-02-09 17:14:15 +0530

    added review expand tool in requested_reviews part

--- src/App.js
+++ src/App.js
@@ -1,51 +1,6 @@
 import "./requested_reviews_styles.css";
+import useCollapse from 'react-collapsed';
 import { React, useState } from "react";
-
-export default function App() {
-  const [cards, setCards] = useState(Cards_array);
-
-  return (
-    <div className="App">
-      <table>
-        <tbody>
-          <th className="left-menu-bar">
-            <tr>
-              <button className="my-profile-text">My Profile</button>
-            </tr>
-            <tr>
-              <th>
-                <button className="review">Review</button>
-              </th>
-            </tr>
-            <tr>
-              <th>
-                <button className="reviews">Reviews</button>
-              </th>
-            </tr>
-            <tr>
-              <th>
-                <button className="requested_reviews">Requested Reviews</button>
-              </th>
-            </tr>
-            <tr>
-              <th>
-                <button className="add_review">Add Review</button>
-              </th>
-            </tr>
-            <tr>
-              <button className="review-results">Review Results</button>
-            </tr>
-          </th>
-          <th className="right-info-page">
-            <tr>
-              <CardList cards = {cards}/>
-            </tr>
-          </th>
-        </tbody>
-      </table>
-    </div>
-  );
-}
 
 const Cards_array = [
   {
@@ -84,6 +39,67 @@ const Cards_array = [
     status: "Completed"
   }
 ];
+
+export default function App() {
+  const [cards, setCards] = useState(Cards_array);
+
+  return (
+    <div className="App">
+      <table>
+        <tbody>
+          <th className="left-menu-bar">
+            <tr>
+              <button className="my-profile-text">My Profile</button>
+            </tr>
+            <Collapsible/>
+            <tr>
+              <button className="review-results">Review Results</button>
+            </tr>
+          </th>
+          <th className="right-info-page">
+            <tr>
+              <CardList cards = {cards}/>
+            </tr>
+          </th>
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+function Collapsible() {
+  const { getCollapseProps, getToggleProps, isExpanded } = useCollapse();
+  return (
+    <div >
+      <div {...getToggleProps()}>
+              {
+              <div>
+                <th>
+              <button className="review">Review</button>
+            </th>
+              </div>
+              }
+          </div>
+          <div {...getCollapseProps()}>
+          <tr>
+            <th>
+              <button className="reviews">Reviews</button>
+            </th>
+          </tr>
+          <tr>
+            <th>
+              <button className="requested_reviews">Requested Reviews</button>
+            </th>
+          </tr>
+          <tr>
+            <th>
+              <button className="add_review">Add Review</button>
+            </th>
+          </tr>
+          </div> 
+    </div>
+  );
+            }
 
 function CardList({cards}) {
   return (
